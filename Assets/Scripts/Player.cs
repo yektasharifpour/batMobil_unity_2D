195df6db0cfe9554fce8f449be0edde1da1e6408
yektasharifpour <154242506+yektasharifpour@users.noreply.github.com>
2025-12-16 18:04:22 +0330

    add trigger to player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,41 +5,65 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private GameObject frontLight;
     [SerializeField] private GameObject backLight;
-
+    [SerializeField] private GameObject batSignal;
     [SerializeField] private Sprite normalLight;
     [SerializeField] private Sprite stealthLight;
     [SerializeField] private Sprite alertBlueLight;
     [SerializeField] private Sprite alertRedLight;
+    [SerializeField] private Transform cam;
+    [SerializeField] private float xOffset = -20f;
+    //[SerializeField] bool turnLightOn = true;
 
     [SerializeField] private float changeColorTime = 1.0f;
 
     private SpriteRenderer frontRenderer;
     private SpriteRenderer backRenderer;
-
     private Coroutine alertRoutine;
+
 
     enum PlayerState { Normal, Stealth, Alert }
     PlayerState currentState;
 
     void Start()
     {
+        //turnLightOn = true;
         frontRenderer = frontLight.GetComponent<SpriteRenderer>();
         backRenderer = backLight.GetComponent<SpriteRenderer>();
         SetState(PlayerState.Normal);
+        batSignal.gameObject.SetActive(false);
+        if (cam == null) cam = Camera.main.transform;
+        //xOffset = transform.position.x - cam.position.x;
+
+
     }
 
+
+
+    void LateUpdate()
+    {
+        if (cam == null) return;
+
+          Vector3 pos = transform.position;
+    pos.x = cam.position.x + xOffset;
+    transform.position = pos;
+    }
+    //کنترل حالت ها و بت سیگنال
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.N)) SetState(PlayerState.Normal);
-        else if (Input.GetKeyDown(KeyCode.S)) SetState(PlayerState.Stealth);
-        else if (Input.GetKeyDown(KeyCode.A)) SetState(PlayerState.Alert);
+        else if (Input.GetKeyDown(KeyCode.C)) SetState(PlayerState.Stealth);
+        else if (Input.GetKeyDown(KeyCode.Space)) SetState(PlayerState.Alert);
+        turnOnBatSignal();
+        //turnOffLight();
+     
     }
 
+    // تابع تخصیص حالت
     void SetState(PlayerState newState)
     {
         if (currentState == newState) return;
 
-        // اگر داریم از Alert خارج می‌شیم، کوروتینش رو قطع کن
+        
         if (currentState == PlayerState.Alert && alertRoutine != null)
         {
             StopCoroutine(alertRoutine);
@@ -50,6 +74,7 @@ public class Player : MonoBehaviour
         UpdateLight();
     }
 
+    // تابع تغییر رنگ چراغ ها بر اساس حالت
     void UpdateLight()
     {
         switch (currentState)
@@ -62,15 +87,47 @@ public class Player : MonoBehaviour
             case PlayerState.Stealth:
                 frontRenderer.sprite = stealthLight;
                 backRenderer.sprite = stealthLight;
+     
                 break;
 
             case PlayerState.Alert:
-                // جلوگیری از چندبار شروع شدن کوروتین
                 if (alertRoutine == null)
                     alertRoutine = StartCoroutine(AlertLightChange());
                 break;
         }
     }
+
+
+    // تابع روشن کردن بت سیگنال
+    void turnOnBatSignal()
+    {
+        if (Input.GetKey(KeyCode.B))
+        {   frontLight.gameObject.SetActive(false);
+            batSignal.gameObject.SetActive(true);
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            frontLight.gameObject.SetActive(true);
+            batSignal.gameObject.SetActive(false);
+        }
+    }
+    //void turnOffLight()
+    //{
+    //    if (!turnLightOn)
+    //    {
+    //        frontLight.gameObject.SetActive(false);
+    //        backLight.gameObject.SetActive(false);
+
+    //    }
+    //    else
+    //    {
+    //        frontLight.gameObject.SetActive(true);
+    //        backLight.gameObject.SetActive(true);
+    //    }
+    //}
+
+
+    // روتین برای چشمک زن کردن چراغ در حالت هشدار
     IEnumerator AlertLightChange()
     {
         while (true)
