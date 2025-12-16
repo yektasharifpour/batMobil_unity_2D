23116f61cfb2eecdb1bd7a9dad85d3ebc7dd05c4
yektasharifpour <154242506+yektasharifpour@users.noreply.github.com>
2025-12-16 10:13:07 +0330

    change background and movement of player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,34 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Users;
 
 public class Player : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private float forwardSpeed = 5f;
+    [SerializeField] private float maxSpeed = 10f;
+    [SerializeField] private float minSpeed;
+    [SerializeField] bool isFast = false;
     void Start()
     {
-        
+        minSpeed = forwardSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
+        float moveForward = Time.deltaTime * forwardSpeed;
+        transform.Translate(moveForward,0,0);
+        if ((Input.GetKey(KeyCode.LeftShift)|| Input.GetKey(KeyCode.RightShift)) && !isFast )
+        {
+            isFast = true;
+            forwardSpeed = maxSpeed;
+        }
+        else
+        {
+            isFast= false;
+            forwardSpeed = minSpeed;
+        }
         
     }
 }
